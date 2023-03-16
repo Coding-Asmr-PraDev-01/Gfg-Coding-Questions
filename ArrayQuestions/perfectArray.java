0589b917ffc190bf180ec9bc15f74be4730553c3
1-0-1PraDev <88771778+1-0-1PraDev@users.noreply.github.com>
2023-03-16 22:39:59 +0530

    Create perfectArray.java

---
+++ ArrayQuestions/perfectArray.java
@@ -0,0 +1,36 @@
+// Brute Force Unoptimized Solution
+public static void perfectArr(int arr[]){
+    int len = arr.length;
+    int newArr[] = new int[len];
+    int isPerfect = 1;
+    for(int i = len - 1; i >= 0; i--){
+      newArr[len - i - 1] = arr[i];  
+    }
+    for(int i = 0; i < len; i++){
+      if(arr[i] != newArr[i]){
+        isPerfect = 0;
+      }
+    }
+    if(isPerfect == 1){
+      System.out.println("PERFECT");
+    }else{
+      System.out.println("NOT PERFECT");
+    }
+  }
+
+// Optimized Solution
+public static Boolean perfect2(int a[]){
+     // Complete the function
+    int n = a.length;
+        boolean isPerfect = false;
+        if(n == 1){
+            return true;
+        }
+        
+        for(int i = 0; i < n/2; i++){
+            isPerfect = false;
+            if(a[i] == a[n-(i+1)])
+                isPerfect = true;
+        }
+      return isPerfect;
+  }
